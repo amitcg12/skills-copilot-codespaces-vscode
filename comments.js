5a83873f7c022797a119b224b7fd35f802b0e8be
amitcg12 <amit.wadhwani@capgemini.com>
2024-12-05 06:06:37 +0000

    Added comments js

--- comments.js
+++ comments.js
@@ -1,25 +1,6 @@
-//create web server
+//Create Web Server
 
-var http = require("http");
+var express = require('express');
+var app = express();
 var fs = require("fs");
-var url = require("url");
-
-var server = http.createServer(function(req, res) {
-	var path = url.parse(req.url).pathname;
-	var filePath = __dirname + path;
-
-	fs.exists(filePath, function(exists) {
-		if(exists) {
-			fs.readFile(filePath, function(err, data) {
-				res.end(data);
-			});
-		} else {
-			res.statusCode = 404;
-			res.end("404 not found");
-		}
-	});
-});
-
-server.listen(8080, function() {
-	console.log("Server is listening on port 8080");
-});
+var bodyParser = require('body-parser');
