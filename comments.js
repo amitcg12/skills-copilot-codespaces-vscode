95b2e555d63aac046a6c88cead4116dae4086401
amitcg12 <amit.wadhwani@capgemini.com>
2024-12-05 06:09:50 +0000

    Added comments.js

--- comments.js
+++ comments.js
@@ -1,6 +1,34 @@
-//Create Web Server
+//Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
 
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var bodyParser = require('body-parser');
+//Create a comment
+app.post('/comments', (req, res) => {
+  res.send('Comment created');
+});
+
+//Read all comments
+app.get('/comments', (req, res) => {
+    res.send('All comments');
+});
+
+//Read a comment
+app.get('/comments/:id', (req, res) => {
+    res.send('Comment with id ' + req.params.id);
+});
+
+//Update a comment
+app.put('/comments/:id', (req, res) => {
+    res.send('Comment with id ' + req.params.id + ' updated');
+});
+
+//Delete a comment
+app.delete('/comments/:id', (req, res) => {
+    res.send('Comment with id ' + req.params.id + ' deleted');
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
+
